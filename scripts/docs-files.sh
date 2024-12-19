aa37a8d1962493d8d474d66058e0c889269b953f
SuChang255 <tsuchang01@gmail.com>
2024-12-19 19:51:33 +0800

    Update docs-files.sh

--- scripts/docs-files.sh
+++ scripts/docs-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Destination folder where all files will be copied
-destination_folder="./doc/files/"
+destination_folder="/doc/files/"
 
 mkdir -p $destination_folder
 # Find all folders named "files" and copy their contents to the destination folder
